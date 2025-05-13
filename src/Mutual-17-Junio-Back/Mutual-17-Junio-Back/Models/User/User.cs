18b481525039483da628fc4a035e3cc356d4b1e9
MatiasCordich <m.scordich@gmail.com>
2025-05-12 22:36:16 -0300

    Se creó el modelo de tabla de Usuario

---
+++ src/Mutual-17-Junio-Back/Mutual-17-Junio-Back/Models/User/User.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mutual_17_Junio_Back.Models.User
+{
+
+    /* -------------------------- Modelo: User [12-05-2025] -------------------------- */
+
+    /* Creacion de la tabla Usuario para la Base de Datos */
+    [Table("Usuario")]
+    public class User
+    {
+
+        /* Primary Key*/
+        [Key] 
+        [Column("id")]
+        public int Id { get; set;}
+
+        [Column("username")]
+        public int Username { get; set; }
+
+        [Column("password_hash")]
+        public int PasswordHash { get; set; }
+
+        [Column("rol")]
+        public int Rol { get; set; }
+
+    }
+}
