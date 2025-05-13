dc735d4b2c1e49417b9fb01df1807359fcb8a2a9
MatiasCordich <m.scordich@gmail.com>
2025-05-12 22:36:59 -0300

    Se creó el modelo para gestionar el Login de Usuarios

---
+++ src/Mutual-17-Junio-Back/Mutual-17-Junio-Back/Models/User/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿namespace Mutual_17_Junio_Back.Models.User
+{
+
+    /* -------------------------- Modelo: UserLogin [12-05-2025] -------------------------- */
+
+    /* Creacion del modelo para el logueo de Usuarios */
+    public class UserLogin
+    {
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+    }
+}
