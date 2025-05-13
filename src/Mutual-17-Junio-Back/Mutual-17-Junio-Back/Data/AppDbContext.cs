3d25ab86319915d5c216404fa0404584eeb81175
MatiasCordich <m.scordich@gmail.com>
2025-05-12 22:35:38 -0300

    Se creo el contexto de conexión para la Base de Datos

---
+++ src/Mutual-17-Junio-Back/Mutual-17-Junio-Back/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Mutual_17_Junio_Back.Models.User;
+
+namespace Mutual_17_Junio_Back.Data
+{
+
+    /* -------------------------- Data: AppDbContext [12-05-2025] -------------------------- */
+
+    /* Creando la conexión entre la API y la Base de Datos */
+    public class AppDbContext : DbContext
+    {
+
+      public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        
+        /* Tablas de la Base de datos */
+        public DbSet<User> Users { get; set; }
+    }
+}
